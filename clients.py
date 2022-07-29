ec5a889dded876a4d087fbda55f1a059e28c82a2
Vicktoriia_Pars <parshia@healbe.com>
2022-07-29 23:48:04 +0300

    finale

--- clients.py
+++ clients.py
@@ -8,15 +8,15 @@ HOST = 'http://127.0.0.1:5000'
 #                         headers={'custom_header_1': 'some_value'})
 
 # response = requests.post(f'{HOST}/users/',
-#                         json={'name': 'user_5', 'password': '12345678'},
+#                         json={'name': 'user_3', 'password': '12345678'},
+#                          )
+# response = requests.put(f'{HOST}/articles/update/24',
+#                         json={'header': 'The forth post', 'description': 'The second post text', 'author': 1},
 #                          )
 # response = requests.post(f'{HOST}/articles/',
-#                         json={'header': 'The second post', 'description': 'The second post text', 'author': 1},
-#                          )
-# response = requests.post(f'{HOST}/articles/',
-#                         json={'header': 'The first post', 'description': 'The first post text', 'author': 12},
-#                          )
+#                         json={'header': 'The third post', 'description': 'The third post text', 'author': 1},
+#                         )
 # response = requests.get(f'{HOST}/users/12')
-# response = requests.get(f'{HOST}/articles/1')
-response = requests.delete(f'{HOST}/articles/delete/1')
+response = requests.get(f'{HOST}/articles/24')
+# response = requests.delete(f'{HOST}/articles/delete/23')
 print(response.text)
--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine, Column, Integer, String, DateTime, func, Text, ForeignKey
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from flask import Flask, jsonify, request
 from flask.views import MethodView
 import pydantic
@@ -23,6 +23,7 @@ class User(BaseModel):
     name = Column(String, index=True, nullable=True, unique=True)
     password = Column(String, nullable=False)
     time = Column(DateTime, server_default=func.now())
+    articles = relationship("Article")
 
 class CreateUserModel(pydantic.BaseModel):
     name: str
@@ -98,7 +99,7 @@ class Article(BaseModel):
 class CreateArticleModel(pydantic.BaseModel):
     header: str
     description: str
-    author = Integer
+    author: int
 
     class Config:
         arbitrary_types_allowed = True
@@ -123,17 +124,20 @@ class ArticleView(MethodView):
             raise HttpError(400, er.errors())
 
         with Session() as session:
+            auth_id = json_data_validate['author']
+            if not session.query(User).get(auth_id):
+                raise HttpError(404, 'user is not valid')
             article = Article(**json_data_validate)
             session.add(article)
             try:
                 session.commit()
                 return jsonify({
-                    'article': article.id
+                    'article': article.id,
+                    'header': article.header,
+                    'description': article.description,
                 })
             except IntegrityError as er:
                 raise HttpError(400, 'such article already exists')
-
-# class ArticleView_UD(MethodView):
 
     def delete(self, article_id):
         with Session() as session:
@@ -141,8 +145,28 @@ class ArticleView(MethodView):
             if article is None:
                 raise HttpError(404, 'article not found')
             else:
-                session.query(Article).delete(article_id)
-            return jsonify({'article deleted'})
+                session.delete(article)
+                session.commit()
+            return jsonify({'result': 'article deleted'})
+
+    def put(self, article_id):
+        try:
+            json_data_validate = CreateArticleModel(**request.json).dict()
+        except pydantic.ValidationError as er:
+            raise HttpError(400, er.errors())
+        with Session() as session:
+            article = session.query(Article).get(article_id)
+            if article is None:
+                raise HttpError(404, 'article not found')
+            if not session.query(User).get(json_data_validate['author']):
+                raise HttpError(404, 'user is not valid')
+            else:
+                article.header = json_data_validate['header']
+                article.description = json_data_validate['description']
+                article.author = json_data_validate['author']
+                session.add(article)
+                session.commit()
+            return jsonify({'result': 'article updated'})
 
 
 BaseModel.metadata.create_all(engine)
@@ -153,7 +177,7 @@ app.add_url_rule('/users/<int:user_id>', view_func=UserView.as_view('get_user'),
 app.add_url_rule('/articles/', view_func=ArticleView.as_view('create_article'), methods=['POST'])
 app.add_url_rule('/articles/<int:article_id>', view_func=ArticleView.as_view('get_article'), methods=['GET'])
 app.add_url_rule('/articles/delete/<int:article_id>', view_func=ArticleView.as_view('delete_article'), methods=['DELETE'])
-
+app.add_url_rule('/articles/update/<int:article_id>', view_func=ArticleView.as_view('update_article'), methods=['PUT'])
 
 
 app.run()
